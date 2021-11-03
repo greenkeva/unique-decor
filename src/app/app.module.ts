10c96bee825b7a5d7585d18d9c1052f756160dac
Shekeva Green <Shekeva.Green@ibm.com>
2021-11-03 15:29:39 -0500

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DecorComponent } from './decor/decor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DecorComponent
   ],
   imports: [
     BrowserModule
