1f86a45c12a052a59cc7ef64100c82b2648bdd89
Shekeva Green <Shekeva.Green@ibm.com>
2021-11-03 16:00:15 -0500

    add decor.module

---
+++ src/app/decor/decor.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DecorComponent } from './decor.component';
+
+
+@NgModule({
+  declarations: [
+    DecorComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [DecorComponent]
+})
+export class AppModule { }
